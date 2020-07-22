79c8467b01c843fc7c1141c20cf3f54d17d3884b
xD4rVision <65313449+xD4rVision@users.noreply.github.com>
2020-07-22 14:35:33 -0300

    Update header.php

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8" />
     <link rel="stylesheet" href="./assets/css/main.css" />
-    <title>SocietySec Cursos</title>
+    <title>Cursos</title>
     <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" />
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet"> 
 </head>
